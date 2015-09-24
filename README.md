477eb8d8edb65614147090767cfbd82023e1a416
Chris Keathley <c@keathley.io>
2015-09-23 20:49:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Butler New
+
+
+This is what a new project looks like that uses butler. Still a work in progress.
---
+++ plugins/example.ex
@@ -0,0 +1,8 @@
+defmodule Example do
+  use Butler.Plugin
+
+  def respond("example me", state) do
+    {:reply, "This is an example response", state}
+  end
+end
+
